3fe988d09e8fc22a309869a4f23b70dd19c60e22
2017FED <sainiparminder2015@gmail.com>
2023-05-06 23:10:30 +1000

    Version 1.0.0

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Resource Location"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage account name"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment either Production or QA"
+}
+
+variable "account_tier" {
+  type        = string
+  description = "Account tier can be Standard or Premium"
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  type        = string
+  description = "Can be LRS or GRS or RAGRS or ZRS or GZRS or RAGRS"
+  default     = "GRS"
+}
